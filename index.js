aea153c532bf8640122e067c00344d88069db38f
Juliana <ju.tartarini@gmail.com>
2019-05-07 10:15:06 -0300

    Backup v.1.0.0

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-function luhn_check(value) {
+function luhnCheck(value) {
   return value.split('')
-      .reverse()
-      .map( (x) => parseInt(x) )
-      .map( (x,idx) => idx % 2 ? x * 2 : x )
-      .map( (x) => x > 9 ? (x % 10) + 1 : x )
-      .reduce( (accum, x) => accum += x ) % 10 === 0;
+    .reverse()
+    .map((x) => parseInt(x))
+    .map((x, idx) => idx % 2 ? x * 2 : x)
+    .map((x) => x > 9 ? (x % 10) + 1 : x)
+    .reduce((accum, x) => accum += x) % 10 === 0;
 }
 
-function date_validate(month,year)
-{
-let now = new Date;
-return (month+"/"+year >= (now.getMonth()+ 1+"/"+now.getFullYear()) ? true:false)
+function dateValidate(month, year) {
+  let now = new Date;
+  return (month + '/' + year >= (now.getMonth() + 1 + '/' + now.getFullYear()) ? true : false);
 }
 
-function cardValidator(cardNumber,cardMonth, cardYear)
-{
-  return (luhn_check(cardNumber) && date_validate(cardMonth, cardYear) ? true: false)
+function cardValidator(cardNumber, cardMonth, cardYear) {
+  return (luhnCheck(cardNumber) && dateValidate(cardMonth, cardYear) ? true : false);
 }
 
-console.log(cardValidator("5201323100722299",05, 2022))
+module.exports = cardValidator;
+
