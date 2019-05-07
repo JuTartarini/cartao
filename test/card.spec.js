04e1d4ea010d710ae2ac52d4575f47d7fccd14ee
Juliana <ju.tartarini@gmail.com>
2019-05-07 17:05:00 -0300

    deu certo test

--- test/card.spec.js
+++ test/card.spec.js
@@ -29,29 +29,29 @@ describe('Retorna true or false para validar números de cartões de credito ', 
 
     describe('Cartões inválidos:', () => {
       it('Vazio', () => {
-        assert.equal(cardValidator.cardValidator(''), false);
+        assert.equal(cardValidator(''), false);
       });
       it('Cartão inválido - 6062825624254005', () => {
-        assert.equal(cardValidator.cardValidator('6062825624254005'), false);
+        assert.equal(cardValidator('6062825624254005'), false);
         //expect(lib.cardValidator("5155901285384518")).to.be.equal(true);
       });
       it('Cartão válido com espaço no início - 6062825624254001', () => {
-        assert.equal(cardValidator.cardValidator(' 6062825624254001'), false);
+        assert.equal(cardValidator(' 6062825624254001'), false);
       });
       it('Cartão válido com espaço no fim 6062825624254001 ', () => {
-        assert.equal(cardValidator.cardValidator('6062825624254001 '), false);
+        assert.equal(cardValidator('6062825624254001 '), false);
       });
       it('Cartão válido com espaço no meio 606282 5624254001', () => {
-        assert.equal(cardValidator.cardValidator('606282 5624254001'), false);
+        assert.equal(cardValidator('606282 5624254001'), false);
       });
       it('Cartão válido com letras a6062825624254001', () => {
-        assert.equal(cardValidator.cardValidator('a6062825624254001'), false);
+        assert.equal(cardValidator('a6062825624254001'), false);
       });
       it('Números inválidos - 0000111122223333', () => {
-        assert.equal(cardValidator.cardValidator('0000111122223333'), false);
+        assert.equal(cardValidator('0000111122223333'), false);
       });
       it('Letras - divirta-se', () => {
-        assert.equal(cardValidator.cardValidator('divirta-se'), false);
+        assert.equal(cardValidator('divirta-se'), false);
       });
     });
   });
